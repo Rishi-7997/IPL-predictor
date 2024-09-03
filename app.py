0123bcf943b993460affcd8bf104136b40487629
Rishi-7997 <rishisharma.308@gmail.com>
2024-09-03 19:40:15 +0530

    added emojis

--- app.py
+++ app.py
@@ -4,26 +4,26 @@ import pickle
 
 # Declaring the teams
 
-teams = ['Sunrisers Hyderabad',
-         'Mumbai Indians',
-         'Royal Challengers Bangalore',
-         'Kolkata Knight Riders',
-         'Kings XI Punjab',
-         'Chennai Super Kings',
-         'Rajasthan Royals',
-         'Delhi Capitals']
+teams = ['🏏 Sunrisers Hyderabad',
+         '🏏 Mumbai Indians',
+         '🏏 Royal Challengers Bangalore',
+         '🏏 Kolkata Knight Riders',
+         '🏏 Kings XI Punjab',
+         '🏏 Chennai Super Kings',
+         '🏏 Rajasthan Royals',
+         '🏏 Delhi Capitals']
 
-# declaring the venues
+# Declaring the venues
 
-cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
-          'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth',
-          'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley',
-          'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
-          'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
-          'Sharjah', 'Mohali', 'Bengaluru']
+cities = ['📍 Hyderabad', '📍 Bangalore', '📍 Mumbai', '📍 Indore', '📍 Kolkata', '📍 Delhi',
+          '📍 Chandigarh', '📍 Jaipur', '📍 Chennai', '📍 Cape Town', '📍 Port Elizabeth',
+          '📍 Durban', '📍 Centurion', '📍 East London', '📍 Johannesburg', '📍 Kimberley',
+          '📍 Bloemfontein', '📍 Ahmedabad', '📍 Cuttack', '📍 Nagpur', '📍 Dharamsala',
+          '📍 Visakhapatnam', '📍 Pune', '📍 Raipur', '📍 Ranchi', '📍 Abu Dhabi',
+          '📍 Sharjah', '📍 Mohali', '📍 Bengaluru']
 
 pipe = pickle.load(open('pipe.pkl', 'rb'))
-st.title('IPL Win Predictor')
+st.title('🏆 IPL Win Predictor')
 
 col1, col2 = st.columns(2)
 
@@ -35,20 +35,20 @@ with col2:
 
 city = st.selectbox('Select the city where the match is being played', sorted(cities))
 
-target = st.number_input('Target')
+target = st.number_input('🎯 Target')
 
 col3, col4, col5 = st.columns(3)
 
 with col3:
-    score = st.number_input('Score')
+    score = st.number_input('🏏 Score')
 
 with col4:
-    overs = st.number_input('Overs Completed')
+    overs = st.number_input('🔄 Overs Completed')
 
 with col5:
-    wickets = st.number_input('Wickets Fallen')
+    wickets = st.number_input('❌ Wickets Fallen')
 
-if st.button('Predict Probability'):
+if st.button('🔮 Predict Probability'):
     runs_left = target - score
     balls_left = 120 - (overs * 6)
     wickets_fallen = 10 - wickets
@@ -71,5 +71,5 @@ if st.button('Predict Probability'):
     lossprob = result[0][0]
     winprob = result[0][1]
 
-    st.header(battingteam + " - " + str(round(winprob * 100)) + "%")
-    st.header(bowlingteam + " - " + str(round(lossprob * 100)) + "%")
+    st.header(battingteam + " - " + str(round(winprob * 100)) + "% 🏆")
+    st.header(bowlingteam + " - " + str(round(lossprob * 100)) + "% ❌")
